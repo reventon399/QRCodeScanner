7f141d2837bfc94314cf54945cba0d33643f98d4
Alexey <alexeylosev1501@gmail.com>
2023-03-11 21:00:16 +0300

    implemented CameraService class.

--- QRCodeScanner/Sources/Services/CameraService.swift
+++ QRCodeScanner/Sources/Services/CameraService.swift
@@ -21,32 +21,84 @@ protocol CameraServiceProtocol: AnyObject {
     var previewLayer: AVCaptureVideoPreviewLayer? { get set }
     
     func cameraSettings()
-    func getCaptureLayer() -> AVCaptureVideoPreviewLayer?
-    func failed()
-    func found(code: String)
+    func getCapturePreviewLayer() -> AVCaptureVideoPreviewLayer?
+    func settingsFailed()
+    func foundQR(code: String)
 }
 
 final class CameraService: NSObject, AVCaptureMetadataOutputObjectsDelegate, CameraServiceProtocol {
     
     weak var delegate: CameraServiceDelegate?
+    
     var captureSession: AVCaptureSession?
     var previewLayer: AVCaptureVideoPreviewLayer?
     
     func cameraSettings() {
-        <#code#>
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        captureSession = AVCaptureSession()
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if ((captureSession?.canAddInput(videoInput)) != nil) {
+            captureSession?.addInput(videoInput)
+        } else {
+            settingsFailed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if ((captureSession?.canAddOutput(metadataOutput)) != nil) {
+            captureSession?.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            settingsFailed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession ?? AVCaptureSession())
+        previewLayer?.videoGravity = .resizeAspectFill
     }
-    
-    func getCaptureLayer() -> AVCaptureVideoPreviewLayer? {
-        <#code#>
+
+    func getCapturePreviewLayer() -> AVCaptureVideoPreviewLayer? {
+        guard let previewLayer = previewLayer else { return nil }
+
+        return previewLayer
     }
-    
-    func failed() {
-        <#code#>
+
+    func settingsFailed() {
+        delegate?.cameraServiceShowAlert(self)
+        captureSession = nil
     }
-    
-    func found(code: String) {
-        <#code#>
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession?.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            foundQR(code: stringValue)
+        }
+
+        delegate?.cameraServiceDismiss(self)
     }
-    
-    
+
+    func foundQR(code: String) {
+        DispatchQueue.main.async {
+            self.delegate?.cameraService(self, foundQRCode: code)
+        }
+    }
+
+    var prefersStatusBarHidden: Bool {
+        return true
+    }
+
 }
