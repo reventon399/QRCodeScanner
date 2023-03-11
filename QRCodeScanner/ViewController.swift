473a75ef3d2473146d68f59dacc9789e5694c52a
Alexey <alexeylosev1501@gmail.com>
2023-03-11 18:11:41 +0300

    Initial Commit

---
+++ QRCodeScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeScanner
+//
+//  Created by Алексей Лосев on 11.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
